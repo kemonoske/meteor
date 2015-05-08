7292fee91f2e4db8735660dde561ebfe060e5530
Bostanica Ion <kemonoske@gmail.com>
2015-05-08 18:30:14 +0300

    Step 5 of tutorial

--- Tutorial/simple-todos/simple-todos.js
+++ Tutorial/simple-todos/simple-todos.js
@@ -5,7 +5,7 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      return Tasks.find({}, {sort: {createdAt: -1}});d
     }
   });
 
@@ -26,6 +26,18 @@ if (Meteor.isClient) {
 
     // Prevent default form submit
     return false;
+  }
+
+}); 
+  
+ // In the client code, below everything else
+ Template.task.events({
+  "click .toggle-checked": function () {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {$set: {checked: ! this.checked}});
+  },
+  "click .delete": function () {
+    Tasks.remove(this._id);
   }
 });
 }
