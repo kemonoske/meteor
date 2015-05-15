831f83c06b5ef756a1531c93c30860cc03037f90
Bostanica Ion <kemonoske@gmail.com>
2015-05-15 18:19:44 +0300

    Step 9

--- Tutorial/simple-todos/simple-todos.js
+++ Tutorial/simple-todos/simple-todos.js
@@ -4,40 +4,81 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});d
+    tasks: function() {
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+      }
+    },
+    hideCompleted: function() {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function() {
+      return Tasks.find({
+        checked: {
+          $ne: true
+        }
+      }).count();
     }
   });
 
   // Inside the if (Meteor.isClient) block, right after Template.body.helpers:
   Template.body.events({
-    "submit .new-task": function (event) {
-    // This function is called when the new task form is submitted
+    "submit .new-task": function(event) {
+      // This function is called when the new task form is submitted
 
-    var text = event.target.text.value;
+      var text = event.target.text.value;
 
-    Tasks.insert({
-      text: text,
-      createdAt: new Date() // current time
-    });
+      Tasks.insert({
+        text: text,
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(), // _id of logged in user
+        username: Meteor.user().username // username of logged in user
+      });
 
-    // Clear form
-    event.target.text.value = "";
+      // Clear form
+      event.target.text.value = "";
 
-    // Prevent default form submit
-    return false;
-  }
+      // Prevent default form submit
+      return false;
+    },
+    // Add to Template.body.events
+    "change .hide-completed input": function(event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
 
-}); 
-  
- // In the client code, below everything else
- Template.task.events({
-  "click .toggle-checked": function () {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {$set: {checked: ! this.checked}});
-  },
-  "click .delete": function () {
-    Tasks.remove(this._id);
-  }
-});
+  // In the client code, below everything else
+  Template.task.events({
+    "click .toggle-checked": function() {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {
+          checked: !this.checked
+        }
+      });
+    },
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
+  });
 }
